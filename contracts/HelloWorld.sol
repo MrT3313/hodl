9ce4e6a6c0bbbb21a2f7bd9f9913a3eaa08dd6a4
Reed Turgeon <mrt@Reeds-MacBook-Pro.local>
2021-12-24 01:03:16 -0500

    Updated: HelloWorld.hello() function to return passed pessage

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,7 +2,19 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract HelloWorld {
-    function hello() public pure returns(string memory) {
-        return "hello world";
+    string public message;
+
+    // constructors
+    constructor(string memory _message) {
+        message = _message;
+    }
+
+    // methods
+    function hello() public view returns(string memory) {
+        return message;
+    }
+
+    function setMessage(string memory _message) public {
+        message = _message;
     }
 }
